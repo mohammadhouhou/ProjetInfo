2c8fd6ca22266e1d656a581f4fb6ed3abbbfa6ce
mohamed <mohamed@LAPTOP-NT6JKDL7>
2020-11-04 13:28:49 +0200

    organized the files and added institution context

---
+++ Projet Informatique/.dal/Entities/InstitutionContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Projet_Informatique.dal.Data
+{
+    public class InstitutionContext :DbContext
+    {
+        public InstitutionContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Institution> institutions { get; set; }
+    }
+}
