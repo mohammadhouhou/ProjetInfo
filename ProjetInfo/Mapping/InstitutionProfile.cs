a72befd2c93e5dcb7ff2f4b24d59c41771fd5b79
Rayokov <elkhouryray@gmail.com>
2020-11-05 22:16:45 +0200

    Added Mapping

---
+++ ProjetInfo/Mapping/InstitutionProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace ProjetInfo.Mapping
+{
+    public class InstitutionProfile : Profile
+    {
+        public InstitutionProfile()
+        {
+            //Source -> Target
+            CreateMap<Insitution, InstitutionReadDto>();
+            //Target -> Source
+            CreateMap<InstitutionReadDto, Institution>();
+            CreateMap<InstitutionUpdateDto, Institution>();
+            CreateMap<Institution, InstitutionUpdateDto>();
+        }
+    }
+}
